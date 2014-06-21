16507cc7e12510053ac5dc0b11c2dbc701aeb7a7
jh-sz <jh-sz@github.com>
2014-06-22 01:28:42 +1000

    draft

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testProjectApp
+ * @description
+ * # testProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testProjectApp', []);
